5a78ef593cfee0d2860d5394e1485af2e836a1b8
Evan Su <evan@hexacyanide.com>
2016-03-14 17:31:14 -0600

    Fix entity tagging.

--- src/com/moddamage/tags/Taggable.java
+++ src/com/moddamage/tags/Taggable.java
@@ -1,6 +1,5 @@
 package com.moddamage.tags;
 
-
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
@@ -22,74 +21,63 @@ public abstract class Taggable<T> {
         this.inner = inner;
     }
 
-    @SuppressWarnings("rawtypes")
-    public static Taggable<?> get(IDataProvider dp, EventInfo info) {
+    public static Taggable<?> get(IDataProvider<?> dp, EventInfo info) {
         if (dp == null) return null;
 
-        {
-            IDataProvider<World> worldDP = DataProvider.transform(World.class, dp, info, false);
-            if (worldDP != null) {
-                return new Taggable<World>(worldDP) {
-                    @Override
-                    protected <D> ITags<D, World> getTags(TagsHolder<D> holder) {
-                        return holder.onWorld;
-                    }
-                };
-            }
+        IDataProvider<World> worldDP = DataProvider.transform(World.class, dp, info, false);
+        if (worldDP != null) {
+            return new Taggable<World>(worldDP) {
+                @Override
+                protected <D> ITags<D, World> getTags(TagsHolder<D> holder) {
+                    return holder.onWorld;
+                }
+            };
         }
 
-        {
-            IDataProvider<OfflinePlayer> playerDP = DataProvider.transform(OfflinePlayer.class, dp, info, false);
-            if (playerDP != null) {
-                return new Taggable<OfflinePlayer>(playerDP) {
-                    @Override
-                    protected <D> ITags<D, OfflinePlayer> getTags(TagsHolder<D> holder) {
-                        return holder.onPlayer;
-                    }
-                };
-            }
+        IDataProvider<Entity> entityDP = DataProvider.transform(Entity.class, dp, info, false);
+        if (entityDP != null) {
+            return new Taggable<Entity>(entityDP) {
+                @Override
+                protected <D> ITags<D, Entity> getTags(TagsHolder<D> holder) {
+                    return holder.onEntity;
+                }
+            };
         }
 
-        {
-            IDataProvider<Entity> entityDP = DataProvider.transform(Entity.class, dp, info, false);
-            if (entityDP != null) {
-                return new Taggable<Entity>(entityDP) {
-                    @Override
-                    protected <D> ITags<D, Entity> getTags(TagsHolder<D> holder) {
-                        return holder.onEntity;
-                    }
-                };
-            }
+        IDataProvider<OfflinePlayer> playerDP = DataProvider.transform(OfflinePlayer.class, dp, info, false);
+        if (playerDP != null) {
+            return new Taggable<OfflinePlayer>(playerDP) {
+                @Override
+                protected <D> ITags<D, OfflinePlayer> getTags(TagsHolder<D> holder) {
+                    return holder.onPlayer;
+                }
+            };
         }
 
-        {
-            IDataProvider<Location> locDP = DataProvider.transform(Location.class, dp, info, false);
-            if (locDP != null) {
-                return new Taggable<Location>(locDP) {
-                    @Override
-                    protected <D> ITags<D, Location> getTags(TagsHolder<D> holder) {
-                        return holder.onLocation;
-                    }
-                };
-            }
+        IDataProvider<Location> locDP = DataProvider.transform(Location.class, dp, info, false);
+        if (locDP != null) {
+            return new Taggable<Location>(locDP) {
+                @Override
+                protected <D> ITags<D, Location> getTags(TagsHolder<D> holder) {
+                    return holder.onLocation;
+                }
+            };
         }
 
-        {
-            IDataProvider<Chunk> chunkDP = DataProvider.transform(Chunk.class, dp, info, false);
-            if (chunkDP != null) {
-                return new Taggable<Chunk>(chunkDP) {
-                    @Override
-                    protected <D> ITags<D, Chunk> getTags(TagsHolder<D> holder) {
-                        return holder.onChunk;
-                    }
-                };
-            }
+        IDataProvider<Chunk> chunkDP = DataProvider.transform(Chunk.class, dp, info, false);
+        if (chunkDP != null) {
+            return new Taggable<Chunk>(chunkDP) {
+                @Override
+                protected <D> ITags<D, Chunk> getTags(TagsHolder<D> holder) {
+                    return holder.onChunk;
+                }
+            };
         }
 
         LogUtil.error(dp.provides().getSimpleName() + " is not a taggable type: "+dp);
         return null;
     }
-    
+
     public <D> D get(Tag<D> tag, EventData data) throws BailException {
     	T innerval = inner.get(data);
     	if (innerval == null) return null;
@@ -137,9 +125,8 @@ public abstract class Taggable<T> {
 
     protected abstract <D> ITags<D, T> getTags(TagsHolder<D> holder);
 
+    @Override
     public String toString() {
         return inner.toString();
-
-        //return "("+ inner.provides().getSimpleName() + ")["+ inner.getClass().getSimpleName() +"]" + inner.toString();
     }
 }
